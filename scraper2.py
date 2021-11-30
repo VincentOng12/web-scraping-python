ab55aa94caf794fbaac08788c7b1a2ad8c16a519
VincentOng12 <88222523+VincentOng12@users.noreply.github.com>
2021-11-30 22:26:49 +0700

    first push

---
+++ scraper2.py
@@ -0,0 +1,55 @@
+from selenium.webdriver import Chrome
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import time
+driver = Chrome(executable_path='C:/Users/Vincent Ong/lamar_kerja_tes_projek/bricks/chromedriver_win32/chromedriver.exe')
+driver.get('https://www.tokopedia.com/p/handphone-tablet/handphone?page=1')
+driver.execute_script("window.scrollTo(0, 1460)")
+
+try:
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME,  "css-13l3l78"))
+    )
+    
+finally:
+    print(" Sukses? ")
+
+print(len(driver.find_elements(By.XPATH, "//div[@class = 'css-bk6tzz e1nlzfl3']")),'<=========')
+
+data = driver.page_source
+driver.quit()
+soup = BeautifulSoup(data, "html.parser")
+# print(soup, "soup =>>>>>>")
+itemLists = soup.findAll("div", class_ = "css-bk6tzz")
+# print(itemLists)
+output = []
+
+for i, item in enumerate(itemLists):
+    rating_counter = 0
+    nama_produk = item.find("span", class_ = "css-1bjwylw").text
+    gambar_produk = item.find("img")['src']
+    harga_produk = item.find("span", class_ = "css-o5uqvq").text
+    nama_toko = item.findAll("span", class_ = "css-1kr22w3")[1].text
+    rating_produk = item.findAll("img", class_ = "css-177n1u3")
+    for i, rating in enumerate(rating_produk):
+        rating_counter += 1
+    # obj = {
+    #     "Nama_Produk": nama_produk,
+    #     "Gambar_Produk": gambar_produk,
+    #     "Harga_Produk": harga_produk,
+    #     "Nama_Toko": nama_toko,
+    #     "Rating_Produk": rating_counter
+    # }
+    # output.append(obj)
+    output.append([nama_produk,gambar_produk,harga_produk,nama_toko,rating_counter])
+print(output, "output nich")
+
+with open('bricksTest.csv', 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    heading = ['Nama Produk','Gambar Produk','Harga Produk','Nama Toko','Rating Produk']
+    csv_writer.writerow(heading)
+    for data in output:
+        csv_writer.writerow(data)
